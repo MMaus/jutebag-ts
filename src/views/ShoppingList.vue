a8cc204666a177bdc6aff9f34e2ff14cf533545b
Moritz Maus <h.moritz.maus@gmail.com>
2020-07-20 21:56:56 +0200

    Refactor ShoppingList to TypeScript

--- src/views/ShoppingList.vue
+++ src/views/ShoppingList.vue
@@ -70,24 +70,25 @@
         />
         <datalist id="categorylist">
           <option v-for="cat in categories" :key="cat.name" :value="cat.name">{{cat.name}}</option>
-          <!-- <option value="foo">foo</option>
-        <option value="bar">bar</option>
-          <option value="baz">baz</option>-->
         </datalist>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Prop, Vue, Ref } from "vue-property-decorator";
+
 import ShoppingCategory from "@/components/ShoppingCategory.vue";
 
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
+import { User } from "firebase/app";
 import itemStore from "../use/itemStore";
+import { Item, Category } from "../use/itemStore";
 
-function createItem(itemName, categoryName, qty) {
+function createItem(itemName: string, categoryName: string, qty: number): Item {
   const anItem = itemStore.createShoppingItem(itemName, categoryName, qty);
   console.log("itemId :" + anItem.id);
   return anItem;
@@ -95,162 +96,176 @@ function createItem(itemName, categoryName, qty) {
 
 const initialItems = itemStore.loadItems();
 
-export default {
-  mounted: function() {
-    firebase.auth().onAuthStateChanged(user => this.checkLogin(user));
-  },
-
-  data: function() {
-    return {
-      user: "Moritz",
-      items: initialItems,
-      showAddItemEnh: false,
-      newCategory: "",
-      someCats: ["Primary", "Secondary", "Tertiary", "Quartary"],
-      loggedIn: false,
-      userEmail : ""
-    };
-  },
-  methods: {
-    /**
-     * Add a new item from the input field to cart
-     */
-    readCategory: function() {
-      let category = this.$refs.categoryList.value;
-      if (!category) {
-        category = this.$refs.categoryText.value;
-      }
-      if (!category) {
-        category = "undefined";
-      }
-      return category;
-    },
-
-    checkLogin: function(user) {
-      if (!user) {
-        this.loggedIn = false;
-      } else {
-        this.loggedIn = user.emailVerified;
-        this.userEmail = user.email;
-      }
-    },
-
-    upload: function() {
-      console.log("going to upload " + this.items.length + " items");
-      const postData = {
-        email : this.userEmail,
-        items : this.items.map(item => itemStore.toStorableItem(item))
-      }
-      console.log("Sending POST with " + JSON.stringify(postData))
-      fetch("/bag/saveBag", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(postData) 
-      })
-        .then(res => res.json())
-        .then(json => console.log("received POST result:" + JSON.stringify(json)))
-        .catch(err => console.log("POST error: " + err));
-    },
-
-    download: function() {
-      const user = firebase.auth().currentUser.email;
-      console.log("querying items for " + user);
-      fetch("/bag/loadBag?user=" + user)
-        .then(res => res.json())
-        .then(json => this.setNewItems(json))
-        .catch(err => console.log(err));
-    },
-
-    setNewItems: function(items) {
-      console.log("received items:" + items);
-      console.log("received items:" + JSON.stringify(items));
-      items.forEach(it => console.log("an item is " + JSON.stringify(it)));
-    },
-
-    addItem: function() {
-      const itemName = this.$refs.newItem.value;
-      if (!itemName) {
-        return;
-      }
-      const category = this.readCategory();
-      this.$refs.newItem.value = "";
-      this.$refs.categoryList.value = "";
-      this.$refs.categoryText.value = "";
-      const newQty = 1; // TODO: parse from string etc.
-      const newShoppingItem = createItem(itemName, category, newQty);
-      this.items.push(newShoppingItem);
-      itemStore.storeItems(this.items);
-      console.log("added 1 " + itemName + " to shopping list");
-      this.$refs.newItem.focus();
-    },
-    deleteItem: function(itemId) {
-      this.items = this.items.filter(item => item.id != itemId);
-      itemStore.storeItems(this.items);
-    },
-    toggleCart: function(item) {
-      item.stored = !item.stored;
-      itemStore.storeItems(this.items);
-    },
-
-    onInputFocus: function() {
-      console.log("input got focus");
-      this.showAddItemEnh = true;
-    },
-    categoryListChange: function(event) {
-      if (event.target.value) {
-        this.$refs.categoryText.value = "";
-      }
-    },
-    onCategoryTextChange: function() {
-      if (this.$refs.categoryText.value) {
-        this.$refs.categoryList.value = "";
-      }
-    },
-    toggleAddItemEnh: function() {
-      this.showAddItemEnh = !this.showAddItemEnh;
-    },
-    onBlur: function() {
-      console.log("blurred");
-    },
-    updateQty: function(item) {
-      const itemId = item.id;
-      console.log("filtering for items with id " + itemId);
-      this.items
-        .filter(anItem => anItem.id == item.id)
-        .forEach(anItem => {
-          console.log("increasing qty of " + anItem.name + ":" + anItem.qty);
-          anItem.qty = item.qty;
-        });
-      itemStore.storeItems(this.items);
-      console.log("item = " + item.name + ":" + item.qty);
-    },
-    updateCategory: function(item) {
-      const itemId = item.id;
-      console.log("filtering for items with id " + itemId);
-      this.items
-        .filter(anItem => anItem.id == item.id)
-        .forEach(anItem => {
-          console.log(
-            "changing categroy of " + anItem.name + " to " + item.category
-          );
-          anItem.category = item.category;
-        });
-      itemStore.storeItems(this.items);
-      console.log("item = " + item.name + ":" + item.qty);
-    }
-  },
-  computed: {
-    categories: function() {
-      return itemStore.splitInCategories(this.items);
-    }
-  },
-  components: {
-    "shopping-category": ShoppingCategory
+@Component({
+  components : {
+    "shopping-category" : ShoppingCategory
   }
-};
+})
+export default class ShopplingList extends Vue {
+  @Ref("categoryList") categoryList!: HTMLInputElement;
+  @Ref("categoryText") categoryText!: HTMLInputElement;
+  @Ref("newItem") newItem!: HTMLInputElement;
+
+  user = "Moritz";
+  items = initialItems;
+  showAddItemEnh = false;
+  newCategory = "";
+  loggedIn = false;
+  userEmail = "";
+
+  mounted() {
+    firebase.auth().onAuthStateChanged(user => this.checkLogin(user));
+  }
+
+  /**
+   * Add a new item from the input field to cart
+   */
+  readCategory() {
+    let category = this.categoryList.value;
+    if (!category) {
+      category = this.categoryText.value;
+    }
+    if (!category) {
+      category = "undefined";
+    }
+    return category;
+  }
+
+  checkLogin(user: User | null) {
+    if (!user) {
+      this.loggedIn = false;
+    } else {
+      this.loggedIn = user.emailVerified;
+      this.userEmail = user.email ?? "<no email>";
+    }
+  }
+
+  upload() {
+    console.log("going to upload " + this.items.length + " items");
+    const postData = {
+      email: this.userEmail,
+      items: this.items.map(item => itemStore.toStorableItem(item))
+    };
+    console.log("Sending POST with " + JSON.stringify(postData));
+    fetch("/bag/saveBag", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(postData)
+    })
+      .then(res => res.json())
+      .then(json => console.log("received POST result:" + JSON.stringify(json)))
+      .catch(err => console.log("POST error: " + err));
+  }
+
+  download() {
+    const user = firebase.auth().currentUser?.email;
+    if (user == null) {
+      console.error("Unknown user, cannot download cart items.");
+      return;
+    }
+    console.log("querying items for " + user);
+    fetch("/bag/loadBag?user=" + user)
+      .then(res => res.json())
+      .then(json => this.setNewItems(json))
+      .catch(err => console.log(err));
+  }
+
+  setNewItems(items: Array<Item>) {
+    console.log("received items:" + items);
+    console.log("received items:" + JSON.stringify(items));
+    items.forEach(it => console.log("an item is " + JSON.stringify(it)));
+  }
+
+  addItem() {
+    const itemName = this.newItem.value;
+    if (!itemName) {
+      return;
+    }
+    const category = this.readCategory();
+    this.newItem.value = "";
+    this.categoryList.value = "";
+    this.categoryText.value = "";
+    const newQty = 1; // TODO: parse from string etc.
+    const newShoppingItem = createItem(itemName, category, newQty);
+    this.items.push(newShoppingItem);
+    itemStore.storeItems(this.items);
+    console.log("added 1 " + itemName + " to shopping list");
+    this.newItem.focus();
+  }
+
+  deleteItem(itemId: number) {
+    this.items = this.items.filter(item => item.id != itemId);
+    itemStore.storeItems(this.items);
+  }
+
+  toggleCart(item: Item) {
+    item.stored = !item.stored;
+    itemStore.storeItems(this.items);
+  }
+
+  onInputFocus() {
+    console.log("input got focus");
+    this.showAddItemEnh = true;
+  }
+
+  categoryListChange(event: Event) {
+    if (event?.target) {
+      this.categoryText.value = "";
+    }
+  }
+
+  onCategoryTextChange() {
+    if (this.categoryText.value) {
+      this.categoryList.value = "";
+    }
+  }
+
+  toggleAddItemEnh() {
+    this.showAddItemEnh = !this.showAddItemEnh;
+  }
+
+  onBlur() {
+    console.log("blurred");
+  }
+
+  updateQty(item: Item) {
+    const itemId = item.id;
+    console.log("filtering for items with id " + itemId);
+    this.items
+      .filter(anItem => anItem.id == item.id)
+      .forEach(anItem => {
+        console.log("increasing qty of " + anItem.name + ":" + anItem.qty);
+        anItem.qty = item.qty;
+      });
+    itemStore.storeItems(this.items);
+    console.log("item = " + item.name + ":" + item.qty);
+  }
+
+  updateCategory(item: Item) {
+    const itemId = item.id;
+    console.log("filtering for items with id " + itemId);
+    this.items
+      .filter(anItem => anItem.id == item.id)
+      .forEach(anItem => {
+        console.log(
+          "changing categroy of " + anItem.name + " to " + item.category
+        );
+        anItem.category = item.category;
+      });
+    itemStore.storeItems(this.items);
+    console.log("item = " + item.name + ":" + item.qty);
+  }
+
+  // "get" makes this a "computed property"
+  get categories() {
+    return itemStore.splitInCategories(this.items);
+  }
+
+}
 </script>
+
 <style scoped>
 .newItems {
   background-color: #ffeaed;
